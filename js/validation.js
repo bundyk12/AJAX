15ed9f53871097700ca7a4fca342817594a20330
Katherine Bundy <KB@katherine-bundy.hsd1.co.comcast.net>
2015-02-26 15:27:44 -0700

    Thus Far

---
+++ js/validation.js
@@ -0,0 +1,45 @@
+//AJAX
+var searchUrl = 'http://api.bing.net/qson.aspx?';
+
+function populateQuery(input) {
+	var ui = input;
+	var url = encodeURI(searchUrl + 'query=' + ui + "&JsonType=callback&JsonCallback=?");
+
+	$.ajax({
+		url: url,
+		dataType: 'jsonp',
+	}).done(function(response){
+		showResults(response.web);
+	});
+};
+
+function showResults(web) {
+	var results = $('.results');
+	results.empty();
+	$(web).each(function(){
+		results.append(webSearchHTML(web.ID));
+	});
+};
+
+function webSearchHTML(web.ID) {
+	var webString = '<div class="search">' +
+					'<div class="sub-menu">' +
+						'<div class="title">' + web.Title + '</div>' +
+						'<div class="description">Rated: ' + web.Description + '</div>' +
+						'<div class="displayUrl">' + '<a href="' + web.Url + '">' + web.DisplayUrl + '</a></div>' +
+					'</div>' +
+				'</div>';
+
+	var listEl = $(webString);
+	return listEl;
+}
+
+
+var userInput = document.getElementById("search");
+userInput.addEventListener("keyup", populateQuery);
+
+
+
+
+
+
